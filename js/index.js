f65cbbf3656cced4a36b0ef48f261a200791725f
Uliana Boiko <clubfate@gmail.com>
2024-01-11 00:53:02 +0200

    Start

---
+++ js/index.js
@@ -0,0 +1,64 @@
+class CoffeeMachine {
+    _on = false;
+
+    constructor(brand, coffeeTypes, waterVolume) {
+        this.brand = brand;
+        this.coffeeTypes = coffeeTypes;
+        this.waterVolume = waterVolume;
+    }
+
+    on() {
+        this._on = true;
+        console.log('-= ON =-');
+    }
+
+    checkOn() {
+        if (!this._on) {
+          console.log('Coffee Machine is OFF !!!');
+        }
+        return this._on;
+    }
+
+    showInfo() {
+        console.log('---------- INFO ---------');
+        console.log(`brand:       ${ this.brand }`);
+        console.log(`coffeeTypes: ${ this.coffeeTypes.join(', ') }`);
+        console.log(`waterVolume: ${ this.waterVolume }л`);
+        console.log('===============');
+    }
+
+    makeCoffee(coffeeType) {
+        if (!this.checkOn()) {
+            return;
+        }
+
+        if (!this.coffeeTypes.includes(coffeeType)) {
+            console.log(`Помилка`);
+            return;
+        }
+
+        console.log('Старт...');
+        console.log(`Ваше ${coffeeType} готово!`);
+    }
+}
+
+const coffeeMach1 = new CoffeeMachine('Philips', ['капучіно', 'латте', 'еспресо'], 3);
+const coffeeMach2 = new CoffeeMachine('Samsung', ['капучіно', 'латте'], 2);
+
+coffeeMach1.showInfo();
+coffeeMach1.on();
+// coffeeMach1.checkOn();
+coffeeMach1.makeCoffee('капучіно');
+coffeeMach1.makeCoffee('xbxfbfg');
+
+coffeeMach2.showInfo();
+coffeeMach2.on();
+// coffeeMach2.checkOn();
+coffeeMach2.makeCoffee('латте');
+
+
+
+
+
+
+
